4d8613844ab059580b19ae5a942d8266372ad2c3
Aditya <adityaofficiall9098@gmail.com>
2026-06-24 11:34:42 +0530

    Fix CORS

--- foodiesapi/src/main/java/in/pradeep/foodiesapi/config/SecurityConfig.java
+++ foodiesapi/src/main/java/in/pradeep/foodiesapi/config/SecurityConfig.java
@@ -74,7 +74,11 @@ public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Excepti
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+       config.setAllowedOrigins(List.of(
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://online-food-delivery-system-ten.vercel.app"
+));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
